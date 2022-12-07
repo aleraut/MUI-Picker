f1680a18e484c424b87261c6e96aa8457c1afb5c
Aleksis Rautiainen <aleraut9@gmail.com>
2022-12-07 22:13:46 +0200

    Added tabs for home and todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import TodoList from './TodoList';
 import TabApp from './TabApp';
 
 function App() {
   return (
     <div className="App">
       <TabApp />
-      <TodoList />
     </div>
   );
 }
